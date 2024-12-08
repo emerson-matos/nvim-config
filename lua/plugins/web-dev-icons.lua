b8608fa9df2963c0e5d5db700b801cd9cfcb0768
Emerson Matos <emegson@live.com>
2024-12-08 18:21:32 -0300

    lsp-config

--- lua/plugins/web-dev-icons.lua
+++ lua/plugins/web-dev-icons.lua
@@ -1,24 +1,35 @@
 return {
-  {
-    "echasnovski/mini.icons",
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons", lazy = true },
-    },
-    lazy = true,
-    opts = {
-      file = {
-        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
-        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
-      },
-      filetype = {
-        dotenv = { glyph = "", hl = "MiniIconsYellow" },
-      },
-    },
-    init = function()
-      package.preload["nvim-web-devicons"] = function()
-        require("mini.icons").mock_nvim_web_devicons()
-        return package.loaded["nvim-web-devicons"]
-      end
-    end,
-  },
+  -- {
+  --   "echasnovski/mini.icons",
+  --   dependencies = {
+  --     { "nvim-tree/nvim-web-devicons", lazy = true },
+  --   },
+  --   lazy = true,
+  --   opts = {
+  --     file = {
+  --       [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+  --       ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+  --       [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+  --       [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+  --       [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+  --       ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+  --       ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+  --       ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["tsconfig.app.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["tsconfig.node.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+  --     },
+  --     filetype = {
+  --       dotenv = { glyph = "", hl = "MiniIconsYellow" },
+  --     },
+  --   },
+  --   init = function()
+  --     package.preload["nvim-web-devicons"] = function()
+  --       require("mini.icons").mock_nvim_web_devicons()
+  --       return package.loaded["nvim-web-devicons"]
+  --     end
+  --   end,
+  -- },
 }
