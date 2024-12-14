1815e5f65515af97e424786534d68e0aa789406f
Emerson Matos <emegson@live.com>
2024-12-14 13:31:54 -0300

    no nix extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.nix" },
+    -- { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
