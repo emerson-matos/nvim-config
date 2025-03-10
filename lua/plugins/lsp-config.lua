3a75dcb426bafe1841aeac4311b2cb82f0c0c311
Emerson Matos <emegson@live.com>
2025-03-10 16:02:41 -0300

    chore: typescript babt

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,6 +16,85 @@ return {
           },
         },
       },
+      -- TypeScript
+      {
+        "pmizio/typescript-tools.nvim",
+        dependencies = {
+          "stevearc/conform.nvim",
+          -- "neovim/nvim-lspconfig",
+          "nvim-lua/plenary.nvim",
+        },
+        ft = {
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+        },
+        opts = {
+          -- spawn additional tsserver instance to calculate diagnostics on it
+          separate_diagnostic_server = true,
+          -- "change"|"insert_leave" determine when the client asks the server about diagnostic
+          publish_diagnostic_on = "insert_leave",
+          -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
+          -- "remove_unused_imports"|"organize_imports") -- or string "all"
+          -- to include all supported code actions
+          -- specify commands exposed as code_actions
+          expose_as_code_action = {},
+          -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
+          -- not exists then standard path resolution strategy is applied
+          tsserver_path = nil,
+          -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
+          -- (see 💅 `styled-components` support section)
+          tsserver_plugins = {},
+          -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
+          -- memory limit in megabytes or "auto"(basically no limit)
+          tsserver_max_memory = "auto",
+          -- described below
+          tsserver_format_options = {
+            insertSpaceAfterCommaDelimiter = true,
+            insertSpaceAfterConstructor = false,
+            insertSpaceAfterSemicolonInForStatements = true,
+            insertSpaceBeforeAndAfterBinaryOperators = true,
+            insertSpaceAfterKeywordsInControlFlowStatements = true,
+            insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+            insertSpaceBeforeFunctionParenthesis = false,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
+            insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
+            insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
+            insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
+            insertSpaceAfterTypeAssertion = false,
+            placeOpenBraceOnNewLineForFunctions = false,
+            placeOpenBraceOnNewLineForControlBlocks = false,
+            semicolons = "insert",
+            indentSwitchCase = true,
+          },
+          tsserver_file_preferences = {},
+          -- locale of all tsserver messages, supported locales you can find here:
+          -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
+          tsserver_locale = "pt-br",
+          -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
+          complete_function_calls = false,
+          include_completions_with_insert_text = true,
+          -- CodeLens
+          -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
+          -- possible values: ("off"|"all"|"implementations_only"|"references_only")
+          code_lens = "all",
+          -- by default code lenses are displayed on all referencable values and for some of you it can
+          -- be too much this option reduce count of them by removing member references from lenses
+          disable_member_code_lens = true,
+          -- JSXCloseTag
+          -- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
+          -- that maybe have a conflict if enable this feature. )
+          jsx_close_tag = {
+            enable = false,
+            filetypes = { "javascriptreact", "typescriptreact" },
+          },
+        },
+      },
     },
     ---@class PluginLspOpts
     opts = {
@@ -34,54 +113,14 @@ return {
         },
         jdtls = {},
         tailwindcss = {},
-        ts_ls = {
-          keys = {
-            {
-              "gD",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "typescript.goToSourceDefinition",
-                  arguments = { params.textDocument.uri, params.position },
-                  open = true,
-                })
-              end,
-              desc = "Goto Source Definition",
-            },
-            {
-              "gR",
-              function()
-                LazyVim.lsp.execute({
-                  command = "typescript.findAllFileReferences",
-                  arguments = { vim.uri_from_bufnr(0) },
-                  open = true,
-                })
-              end,
-              desc = "File References",
-            },
-            {
-              "<leader>co",
-              LazyVim.lsp.action["source.organizeImports"],
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cM",
-              LazyVim.lsp.action["source.addMissingImports.ts"],
-              desc = "Add missing imports",
-            },
-            {
-              "<leader>cu",
-              LazyVim.lsp.action["source.removeUnused.ts"],
-              desc = "Remove unused imports",
-            },
-            {
-              "<leader>cD",
-              LazyVim.lsp.action["source.fixAll.ts"],
-              desc = "Fix all diagnostics",
-            },
-          },
-        },
+        eslint = {},
         vtsls = false,
+      },
+      setup = {
+        vtsls = function()
+          -- disable tsserver
+          return true
+        end,
       },
     },
   },
