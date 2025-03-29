0e1082a69977bf1602731ada6fb6f3e232b10f50
Emerson Matos <emegson@live.com>
2025-03-29 01:24:22 -0300

    chore: flash

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,42 +1,42 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {extensions = { fzf = {} }},
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>,",
-        function() require("telescope.builtin").buffers({sort_mru=true, sort_lastused=true}) end,
-        desc = "Switch Buffer",
-      },
-      {
-        "<leader>fh",
-        function() require("telescope.builtin").help_tags() end,
-        desc = "Switch Buffer",
-      },
-
-      {
-        "<leader>/",
-        function()
-          require("telescope.builtin").live_grep()
-        end,
-        desc = "Grep (Root Dir)",
-      },
-      {
-        "<leader>:",
-        function()
-          require("telescope.builtin").command_history()
-        end,
-        desc = "Command History",
-      },
-      {
-        "<leader><space>",
-        function()
-          require("telescope.builtin").find_files()
-        end,
-        desc = "Find Files (Root Dir)",
-      },
-    },
-    dependencies = { "nvim-lua/plenary.nvim", {"nvim-telescope/telescope-fzf-native.nvim", build = "make"}},
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   opts = {extensions = { fzf = {} }},
+  --   event = "VeryLazy",
+  --   keys = {
+  --     {
+  --       "<leader>,",
+  --       function() require("telescope.builtin").buffers({sort_mru=true, sort_lastused=true}) end,
+  --       desc = "Switch Buffer",
+  --     },
+  --     {
+  --       "<leader>fh",
+  --       function() require("telescope.builtin").help_tags() end,
+  --       desc = "Switch Buffer",
+  --     },
+  --
+  --     {
+  --       "<leader>/",
+  --       function()
+  --         require("telescope.builtin").live_grep()
+  --       end,
+  --       desc = "Grep (Root Dir)",
+  --     },
+  --     {
+  --       "<leader>:",
+  --       function()
+  --         require("telescope.builtin").command_history()
+  --       end,
+  --       desc = "Command History",
+  --     },
+  --     {
+  --       "<leader><space>",
+  --       function()
+  --         require("telescope.builtin").find_files()
+  --       end,
+  --       desc = "Find Files (Root Dir)",
+  --     },
+  --   },
+  --   dependencies = { "nvim-lua/plenary.nvim", {"nvim-telescope/telescope-fzf-native.nvim", build = "make"}},
+  -- },
 }
--- lua/plugins/vim-sleuth.lua
+++ lua/plugins/vim-sleuth.lua
@@ -3,5 +3,6 @@ return  {
     "tpope/vim-sleuth",
     opts = {},
     event = { 'BufReadPre', 'BufNewFile' },
+    config = function () end,
   }
 }
