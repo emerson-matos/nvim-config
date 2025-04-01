49c1cabade2a954ad23c1528e42c0b8cbd54bd8b
Emerson Matos <emegson@live.com>
2025-04-01 16:06:24 -0300

    chore: session rocks

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  },
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end,                desc = "Load the session for the current directory" },
+    { "<leader>qS", function() require("persistence").select() end,              desc = "Select a session to load" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+  },
+}
